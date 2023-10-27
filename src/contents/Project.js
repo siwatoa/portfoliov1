5d8d195ef66cb8663ea2fb1ca43572a4ab8f2268
siwatoa <siwatoa@gmail.com>
2023-10-28 03:01:02 +0700

    edit project page

--- src/contents/Project.js
+++ src/contents/Project.js
@@ -30,9 +30,9 @@ export const ProjectDev = [
 
 export const ProjectData = [
    {img: travelpic,
-       name: "Build the Fitness Website (Group Project)",
-       des1: "• Implemented the website, following the final project sprint provided by Generation Thailand.",
-       des2: "• Ultilized MERN Full Stack Skills to develop the website.",
+       name: "Airline Passenger Satisfaction",
+       des1: "• Identified significant factors affecting passenger satisfaction from services or products",
+       des2: "• Utilized Python libraries to analyze and visualize data",
        link1: travelDoc,
        link2: "https://colab.research.google.com/drive/1ALPc6FrxzOwQRhCM2aWc_yt0BRQVQhDt#scrollTo=Nj5OsgFVzGF9",
     },
